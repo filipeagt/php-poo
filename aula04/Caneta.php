b9c37aee93cae6bd950404ec05feeb541a6b2ee4
Filipe Almeida <filipeagt.92@gmail.com>
2024-07-23 20:18:26 -0300

    Update Caneta.php

--- aula04/Caneta.php
+++ aula04/Caneta.php
@@ -1,39 +1,56 @@
-<?php 
-    class Caneta {
-        private $modelo;
-        private $cor;
-        private $ponta;
-        private $tampada;
+<?php
+class Caneta
+{
+    private $modelo;
+    private $cor;
+    private $ponta;
+    private $tampada;
 
-        public function __construct($m, $c ,$p) {
-            $this->modelo = $m;
-            $this->cor = $c;
-            $this->ponta = $p;
-            $this->tampar();
-        }
-
-        public function tampar() {
-            $this->tampada = true;
-        }
-
-        public function destampar() {
-            $this->tampada = false;
-        }
-
-        public function getModelo() {
-            return $this->modelo;
-        }
-
-        public function setModelo($m) {
-            $this->modelo = $m;
-        }
-
-        public function getPonta() {
-            return $this->ponta;
-        }
-
-        public function setPonta($p) {
-            $this->ponta = $p;
-        }
+    public function __construct($modelo,  $cor,  $ponta)
+    {
+        $this->modelo = $modelo;
+        $this->cor = $cor;
+        $this->ponta = $ponta;
+        $this->tampada = true;
     }
-?>
+
+    public function getModelo()
+    {
+        return $this->modelo;
+    }
+
+    public function getCor()
+    {
+        return $this->cor;
+    }
+
+    public function getPonta()
+    {
+        return $this->ponta;
+    }
+
+    public function getTampada()
+    {
+        return $this->tampada;
+    }
+
+    public function setModelo($modelo)
+    {
+        $this->modelo = $modelo;
+    }
+
+    public function setCor($cor)
+    {
+        $this->cor = $cor;
+    }
+
+    public function setPonta($ponta)
+    {
+        $this->ponta = $ponta;
+    }
+
+    public function setTampada($tampada)
+    {
+        $this->tampada = $tampada;
+    }
+}
